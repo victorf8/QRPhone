69bc2dc50389844e715db2ed2dcc1dadf0985163
Victor Ferri <victorferri98z@gmail.com>
2022-04-22 11:36:48 -0700

    now post insnt working consistently in thread

--- app/src/main/java/com/pbaileyapps/qrscannertest/MainActivity.kt
+++ app/src/main/java/com/pbaileyapps/qrscannertest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pbaileyapps.qrscannertest
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView:TextView = findViewById(R.id.textView)
+        val textView: EditText = findViewById(R.id.eventCode)
         val qrButton:ImageButton = findViewById(R.id.qr_button)
         qrButton.setOnClickListener({
             val intentIntegrator = IntentIntegrator(this)
@@ -30,70 +31,9 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         val result = IntentIntegrator.parseActivityResult(resultCode, data)
         if (result != null) {
-//            val url = URL("https://victorf8.pythonanywhere.com/submit-attendance")
-//            val postData = "${result.contents}"
-//
-//            val conn = url.openConnection()
-//            conn.doOutput = true
-//            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
-//            conn.setRequestProperty("Content-Length", postData.length.toString())
-//
-//            Log.d("tag","we are herrrrre\n")
-//
-//            DataOutputStream(conn.getOutputStream()).use { it.writeBytes(postData) }
-//            BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
-//                var line: String?
-//                while (bf.readLine().also { line = it } != null) {
-//                    Log.d("tag","0 " + line)
-//                    println(line)
-//                }
-//            }
-
-//            Log.d("tag","we are herrrrre1\n")
-//
-//            val message = "${result.contents}"
-//            val serverURL: String = "https://victorf8.pythonanywhere.com/submit-attendance"
-//            val url = URL(serverURL)
-//            val connection = url.openConnection() as HttpsURLConnection
-//            connection.requestMethod = "POST"
-//            connection.connectTimeout = 300000
-//            connection.doOutput = true
-//
-//            Log.d("tag","we are herrrrre2\n")
-//
-//            val postData: ByteArray = message.toByteArray(StandardCharsets.UTF_8)
-//
-//            connection.setRequestProperty("charset", "utf-8")
-//            connection.setRequestProperty("Content-length", postData.size.toString())
-//            connection.setRequestProperty("Content-Type", "application/json")
-//
-//            Log.d("tag","we are herrrrre3\n")
-//
-//            try {
-//                val outputStream: DataOutputStream = DataOutputStream(connection.outputStream)
-//                outputStream.write(postData)
-//                outputStream.flush()
-//            } catch (exception: Exception) {
-//
-//            }
-//
-//            Log.d("tag","we are herrrrre4\n")
-//
-//            if (connection.responseCode != HttpsURLConnection.HTTP_OK && connection.responseCode != HttpsURLConnection.HTTP_CREATED) {
-//                try {
-//                    val inputStream: DataInputStream = DataInputStream(connection.inputStream)
-//                    val reader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
-//                    val output: String = reader.readLine()
-//
-//                    println("There was error while connecting the chat $output")
-//                    System.exit(0)
-//
-//                } catch (exception: Exception) {
-//                    throw Exception("Exception while push the notification  $exception.message")
-//                }
-//            }
 
             thread (start = true){
+
                 val url = URL("https://victorf8.pythonanywhere.com/submit-attendance")
                 val postData = "${result.contents}"
 
@@ -114,7 +54,10 @@ class MainActivity : AppCompatActivity() {
                         println(line)
                     }
                 }
+
             }
+
+
 
         }
     }
